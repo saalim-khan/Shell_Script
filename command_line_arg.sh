0a23d2434f71c917edb33207610eba88f7cb24a5
saalim <ksaalim418@gmail.com>
2024-06-29 10:27:00 +0530

    command line arguments

---
+++ command_line_arg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#command line arguments
+
+name=${1}
+age=${2}
+
+echo "my name is ${name} and my age is ${age}"
+ # when we are entering a command in terminal for eg= ./command_line.sh saalim 23 (afterspace we have to give value)

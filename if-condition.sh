32f7fa2a741f7fa0cecf387354d6605cf5757979
saalim <ksaalim418@gmail.com>
2024-07-01 11:02:34 +0530

    if condition in shell-scripting

---
+++ if-condition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if echo "hello there"                  # when if block execute
+then                                   #what is return in then statement 
+    echo "command ran sucessfully"     #it will run till fi
+fi                                     #
+#echo "not executed sucessfully"

261be3449756e0c79a5ffebca8069706bb4f26b3
saalim <ksaalim418@gmail.com>
2024-07-04 12:02:13 +0530

    if not statement in shell scripting

---
+++ if-not.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name="saalim khan"
+othername="khan Saalim"
+
+if [[ ! ${name} == ${othername} ]]  #for if not use this ------> !
+#this check both string are equal or not(==)
+then   
+    echo "both string are equal"   #use [[ double square bracket]] advance version 
+fi
